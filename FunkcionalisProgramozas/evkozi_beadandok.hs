9b06151be527f161b765644d456f6c487687bae4
Bezzeg A. Benjamin <b.benn2366@gmail.com>
2020-11-19 18:49:41 +0100

    Create evkozi_beadandok.hs

---
+++ FunkcionalisProgramozas/evkozi_beadandok.hs
@@ -0,0 +1,25 @@
+{-1-}
+triangleSides x y z = (x + y > z) && (x + z > y) && (y + z > x)
+
+{-2-}
+fractionalPart x = realToFrac ( x - fromIntegral ( truncate ( x ) ) ) :: Float 
+
+{-3-}
+isPrime :: Int -> Bool
+isPrime x = (length [n | n <- [2 .. x-1], mod x n == 0]) == 0 :: Bool
+primesUntil :: Int -> [Int]
+primesUntil x = [n | n <- [2 .. x-1], isPrime ( n )]
+
+{-4-}
+isLeapYear :: Integer -> Bool
+isLeapYear x = ( mod x 4 == 0 && not (mod x 100 == 0) ) || mod x 400 == 0 :: Bool
+
+{-5-}
+import Data.Char
+toUpperFirst (x:xs) = toUpper x : xs
+toUpperLongs xs = unwords [ if length w > 3 then toUpperFirst w else w | w <-words xs]
+
+{-6-}
+slice :: [Int] -> [a] -> [[a]]
+slice [] n = []
+slice (n:ns) l = take n l : slice ns (drop n l)
